db664f318e27b95c7bad761072d0c2ff03f15347
atria0 <139398099+atria0@users.noreply.github.com>
2026-08-24 11:58:13 +0800

    Update exec.c
    add ksu support

--- fs/exec.c
+++ fs/exec.c
@@ -1706,6 +1706,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execve(int *fd, const char *filename,
+				void *argv, void *envp, int *flags);
+#endif
+
 #ifdef CONFIG_KSU
 extern bool ksu_execveat_hook __read_mostly;
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
@@ -1726,6 +1732,10 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 #ifdef CONFIG_KSU
 	if (unlikely(ksu_execveat_hook))
