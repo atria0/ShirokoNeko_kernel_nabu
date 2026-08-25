93e5e0de4fa800d2f52ade35981bcab021100115
atria0 <139398099+atria0@users.noreply.github.com>
2026-08-25 16:46:07 +0800

    Update exec.c
    backquit if not work

--- fs/exec.c
+++ fs/exec.c
@@ -1728,11 +1728,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
-#ifdef CONFIG_KSU
-	extern int ksu_handle_execveat(int *, struct filename **, void *, void *, int *);
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
-
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
